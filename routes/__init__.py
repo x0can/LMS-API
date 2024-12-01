4177a73a7d89164c83559c7bee3f9143f8629ed7
x0can <alexmwaura43@gmail.com>
2024-12-01 21:04:28 +0300

    fix bug

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,9 +1,9 @@
 from routes.users import user_routes
 from routes.course import course_routes
-from routes.forms import forms_routes
+from routes.forms import form_routes
 
 
 def configure_routes(app):
     app.register_blueprint(user_routes)
     app.register_blueprint(course_routes)
-    app.register_blueprint(forms_routes)
+    app.register_blueprint(form_routes)
