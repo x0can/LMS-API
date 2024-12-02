99479780e8ca7e23a4d2ce932fc21c7d8d44d6e3
x0can <alexmwaura43@gmail.com>
2024-12-02 17:53:16 +0300

    add main

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 from db.course_operations_faker import (
     create_sample_enrollments, create_sample_grades, create_sample_students, create_sample_subjects)
 from models.course_sql_operations import CourseOperations
-from db.connection import get_connection
-import sqlite3
 from flask import Flask
-from faker import Faker
 from routes import configure_routes
 from config import Config
-# from db.student_operations import StudentOperations
 from db.student_queries import get_student_performance
-import random
+
+
 # Initialize Flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Initialize Faker
-fake = Faker()
 
-# Database setup
 
-# Functions to populate the database
 
 # Configure routes
 configure_routes(app)
 
+from db.connection import get_connection
 
 @app.route("/api/v1/report", methods=["GET"])
 def fetch_report():
@@ -61,6 +55,8 @@ if __name__ == "__main__":
     create_sample_subjects()
     create_sample_enrollments(10, 5)
     create_sample_grades(10, 5)
+    
+    print("Database with sample data created")
 
     course_operations.close()
     app.run(debug=True)
